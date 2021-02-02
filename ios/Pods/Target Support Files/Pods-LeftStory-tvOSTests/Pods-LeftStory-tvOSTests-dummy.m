2804074fcca4ecf67db5ac15ed560c68164e76f4
tuna97-comartek <tuna@comartek.vn>
2021-02-03 00:20:07 +0700

    main

---
+++ ios/Pods/Target Support Files/Pods-LeftStory-tvOSTests/Pods-LeftStory-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LeftStory_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_LeftStory_tvOSTests
+@end
---
+++ node_modules/react-native-fast-image/android/build/generated/source/buildConfig/debug/com/dylanvann/fastimage/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dylanvann.fastimage;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.dylanvann.fastimage";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
